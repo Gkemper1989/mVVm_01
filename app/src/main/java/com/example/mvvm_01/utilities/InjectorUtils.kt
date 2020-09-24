95aa1332ed6e17cc5b1f9848be5e4ef533d0c5ce
Gustavo <g.kemperweber@gmail.com>
2020-09-24 13:55:51 +0100

    This is my first commit

--- app/src/main/java/com/example/mvvm_01/utilities/InjectorUtils.kt
+++ app/src/main/java/com/example/mvvm_01/utilities/InjectorUtils.kt
@@ -4,7 +4,7 @@ import com.example.mvvm_01.data.FakeDataBase
 import com.example.mvvm_01.data.QuoteRepository
 import com.example.mvvm_01.ui.quotes.QuotesViewModelFactory
 
-class InjectorUtils
+object InjectorUtils
 {
     fun provideQuotesViewModelFactory(): QuotesViewModelFactory {
         val quoteRepository = QuoteRepository.getInstance(FakeDataBase.getInstance().quoteDao)
