0c7ec5ea40e12452e68d455c9bcf703d24ca804b
Gustavo <g.kemperweber@gmail.com>
2020-09-24 12:59:17 +0100

    This is my first commit

---
+++ app/src/main/java/com/example/mvvm_01/data/Quote.kt
@@ -0,0 +1,9 @@
+package com.example.mvvm_01.data
+
+class Quote (val quoteText: String, val author: String)
+{
+    override fun toString(): String
+    {
+    return "@quoteText - @autor"
+    }
+}
